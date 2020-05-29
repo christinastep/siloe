5c3558036a5f0ce1077bce4d638c2f7de4a9afcf
christina <christina@datagif.fr>
2020-05-29 18:54:40 +0200

    add contact

--- style.js
+++ style.js
@@ -4,6 +4,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let headerLogo=document.querySelector(".header__logo");
   let header=document.querySelector(".header");
 
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+  var openContact = document.querySelectorAll('[data-contact="open"]');
+  var contactContainer =document.querySelector(".contact")
+
+  var buttonContact= document.querySelector(".button--close");
+  for (let i = 0; i < openContact.length; i++) {
+    const openContactButton = openContact[i];
+
+    openContactButton.addEventListener("click",function(){
+      contactContainer.style.transform="translateY(0)";
+    })
+    buttonContact.addEventListener("click",function(){
+      contactContainer.style.transform="translateY(-100%)"
+    })
+  }
+
+  for (let i = 0; i < headerLinks.length; i++) {
+    const headerLink = headerLinks[i];
+    headerLink.addEventListener("click",function(){
+      header.style.padding="10px 20px 10px 20px"
+      headerLogo.style.stroke="#000";
+      header.style.backgroundColor="#fff";
+      for (let i = 0; i < headerLinks.length; i++) {
+        const headerLink = headerLinks[i];
+        headerLink.style.color="#000"
+      }
+    })
+    
+  }
+
   if(!!window.IntersectionObserver){
 
     let options = {
